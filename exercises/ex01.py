7b922b607ba240de34b0108e28c8369ff541817d
Dulio Denis <dulio.denis@yahoo.com>
2014-12-21 09:12:21 -0500

    Exercise 1

---
+++ exercises/ex01.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Print another line."
+#print "This line will not print."
+print "But this one will."
