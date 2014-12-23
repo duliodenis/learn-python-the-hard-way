948a1f270fa18ed2f41ae809c284a87617c0ceb0
Dulio Denis <dulio.denis@yahoo.com>
2014-12-23 13:44:47 -0500

    Exercise 3

---
+++ exercises/ex03.py
@@ -0,0 +1,42 @@
+#
+#  ex03.py
+#  Python the Hard Way Exercise #3
+#
+#  Created by Dulio Denis on 12/23/14.
+#  Copyright (c) 2014 ddApps. All rights reserved.
+# ------------------------------------------------
+#
+
+# This line just prints the string
+print "I will now count my chickens."
+
+#The below lines print out the string and the result of the arithmetic computation
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100 - 25 * 3.0 / 4.0
+
+# Another string only print out
+print "Now I will count the eggs:"
+
+# This line prints out the result of the arithmetic computation on a new line
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6
+
+# This line just prints the string
+print "Is it true that 3 + 2 < 5 - 7 ?" 
+
+# This line prints the result of the computation
+print 3 + 2 < 5 - 7
+
+# These lines prints the string and the result in the same output line
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# Another string only print out
+print "Oh, that's why it's False."
+
+# Another string only print out
+print "How about some more?"
+
+# These lines prints the string and the result in the same output line
+print "Is it greater than?", 5 > -2
+print "Is it greater than or equal to?", 5 >= -2
+print "Is it less than or equal to?", 5 <= -2
---
+++ exercises/ex03b.py
@@ -0,0 +1,17 @@
+#
+#  ex03.py
+#  Python the Hard Way Exercise #3B
+#
+#  Created by Dulio Denis on 12/23/14.
+#  Copyright (c) 2014 ddApps. All rights reserved.
+# ------------------------------------------------
+#  Study Drill Question 3: 
+#  Find something you need to calculate and write a new .py fi le that does it. 
+
+# import the Python math Standard Library
+import math
+
+print "Let's calculate the Area of a Circle with radius of 5."
+
+# Area = pi x radius squared
+print math.pi * pow(5, 2)
