7aeb7b7a4080bf3a3da35a95b6cb7abf0a79e78b
Dulio Denis <dulio.denis@yahoo.com>
2014-12-27 09:12:17 -0500

    Exercise 7

---
+++ exercises/ex07.py
@@ -0,0 +1,33 @@
+#
+#  ex07.py (More Printing)
+#  Python the Hard Way Exercise #7
+#
+#  Created by Dulio Denis on 12/27/14.
+#  Copyright (c) 2014 ddApps. All rights reserved.
+# ------------------------------------------------
+# http://learnpythonthehardway.org/book/ex7.html
+#
+# Printing Strings
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # what'd that do?
+
+# Set-up character strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing to see what happens
+# a newline is inserted without it
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
