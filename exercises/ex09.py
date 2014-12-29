1133e71b4ed864922740f36fd370f0fa62589c56
Dulio Denis <dulio.denis@yahoo.com>
2014-12-29 12:16:34 -0500

    Exercise 9

---
+++ exercises/ex09.py
@@ -0,0 +1,20 @@
+#
+#  ex09.py (Printing, Printing, Printing)
+#  Python the Hard Way Exercise #9
+#
+#  Created by Dulio Denis on 12/29/14.
+#  Copyright (c) 2014 ddApps. All rights reserved.
+# ------------------------------------------------
+# http://learnpythonthehardway.org/book/ex9.html
+#
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days:", days
+print "Here are the months:", months
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
